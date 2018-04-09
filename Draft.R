dd14626102b3fee632c85a151a531fa1bd373a0a
couyang24 <30837746+couyang24@users.noreply.github.com>
2018-04-09 12:31:58 -0400

    cont

--- Draft.R
+++ Draft.R
@@ -1,10 +1,40 @@
-if (!require("pacman")) install.packages("pacman")
-pacman::p_load(knitr, tidyverse, data.table, lubridate, zoo, DescTools, lightgbm)
-set.seed(84)               
-options(scipen = 9999, warn = -1, digits= 4)
+library(pacman)
+p_load(xgboost,tidyverse,data.table)
+
+train_sample <- fread("train_sample.csv", 
+               select =c("ip", "app", "device", "os", "channel", "click_time", "is_attributed"),
+               colClasses=c("ip"="numeric","app"="numeric","device"="numeric",
+                            "os"="numeric","channel"="numeric","click_time"="character",
+                            "is_attributed"="numeric"))
+test <- fread("test.csv", 
+              select =c("ip", "app", "device", "os", "channel", "click_time"),
+              showProgress=F,
+              colClasses=c("ip"="numeric","app"="numeric","device"="numeric",
+                           "os"="numeric","channel"="numeric",
+                           "click_time"="character"))
 
 
-train_sample <- read_csv("C:/Users/oouyang/Projects/TalkingData/train_sample.csv")
-train_sample %>% summary()
+train_sample %>% str()
 
-train_col_names %>% train_sample %>% colnames()
+train_sample$click_time <- train_sample$click_time %>% as.Date()
+
+train_sample$click_time %>% summary()
+
+train_sample %>% filter(is_attributed==1) %>% count()
+train_sample %>% filter(is_attributed==0) %>% count()
+
+val <- which(grepl("11-09", train_sample$click_time) == T)
+val <- sample(val, 2 * 10 ^ 2)
+
+pos <- which(train_sample$is_attributed == 1)
+pos <- setdiff(pos, val)
+
+neg <- which(train_sample$is_attributed == 0)
+neg <- setdiff(neg, val)
+neg <- sample(neg, length(pos))
+
+under <- train_sample[c(pos, neg), ]
+valid <- train_sample[val, ]
+
+rm(train_sample)
+gc(reset = T)
