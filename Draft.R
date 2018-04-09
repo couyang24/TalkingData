13ebe4e7d368924be39b878e062d3f0c689640c0
Chengran Ouyang <ouyangchengran@gmail.com>
2018-04-08 23:13:32 -0400

    R update

--- Draft.R
+++ Draft.R
@@ -1,4 +1,10 @@
-library(tidyverse)
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(knitr, tidyverse, data.table, lubridate, zoo, DescTools, lightgbm)
+set.seed(84)               
+options(scipen = 9999, warn = -1, digits= 4)
+
+
 train_sample <- read_csv("C:/Users/oouyang/Projects/TalkingData/train_sample.csv")
 train_sample %>% summary()
 
+train_col_names %>% train_sample %>% colnames()
