824e178cc66d7697c0acdacfb7181b9cc0743248
couyang24 <30837746+couyang24@users.noreply.github.com>
2018-05-02 16:54:35 -0400

    update

--- forked svm, rpart, randomforest.R
+++ forked svm, rpart, randomforest.R
@@ -39,3 +39,43 @@ train <- train %>% mutate(weekdays = as.factor(weekdays(click_time)),
                           hour    = hour(click_time)+1)
 
 train %>% head()
+
+
+
+
+# InTrain
+inTrain <- createDataPartition(train$ip, p=.7, list=F)
+train_val <- train[inTrain,]
+valid_val <- train[-inTrain,]
+rm(inTrain)
+
+train_val$attributed_time <- NULL
+train_val$is_attributed <- as.factor(train_val$is_attributed)
+
+
+# rpart
+rpart_model <- rpart(is_attributed~., data=train_val, method = "class")
+rpart.plot(rpart_model,extra =  3,fallen.leaves = T)
+rpart_pred  <- predict(rpart_model, newdata = valid_val, type="class")
+rpart_pred %>% table()
+
+confusionMatrix(valid_val$is_attributed, rpart_pred)
+rm(rpart_model,rpart_pred)
+
+
+# randomForest
+rf_model <- randomForest(is_attributed~., data=train_val, method = "class")
+rf_pred  <- predict(rf_model, newdata = valid_val, type="class")
+confusionMatrix(valid_val$is_attributed, rf_pred)
+varImpPlot(rf_model)
+rm(rf_model,rf_pred)
+
+
+# svm
+valid_val_svm <- valid_val
+valid_val_svm$is_attributed <- NA
+svm_model <- svm(is_attributed~., data = train_val, method = "class")
+svm_pred  <- predict(svm_model, newdata = valid_val_svm, type ="class")
+confusionMatrix(valid_val$is_attributed, svm_pred)
+rm(svm_model,svm_pred)
+
